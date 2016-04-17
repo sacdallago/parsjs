a719e5a87de145732607a230d6c06da4faeaa9a0
Christian Dallago <code@dallago.us>
2016-04-17 17:36:27 +0200

    Changed . to _ in json output, otherwise you cannot insert in case: column column.something

--- dataGridRenderer.js
+++ dataGridRenderer.js
@@ -151,7 +151,7 @@ module.exports = {
                     var rowOutput = '"' + ( row[j] || "" ) + '"';
                 };
 
-                outputText += ('"'+headerNames[j] +'"' + ":" + rowOutput );
+                outputText += ('"'+headerNames[j].replace(/\./g,"_") +'"' + ":" + rowOutput );
 
                 if (j < (numColumns-1)) {outputText+=","};
             };
